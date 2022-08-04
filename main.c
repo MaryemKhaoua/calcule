7044ff978f260bd5da9084051822b78ff5d1316a
Maryem khaoua <71089834+MaryemKhaoua@users.noreply.github.com>
2022-08-04 16:27:05 +0100

    Add files via upload
    new modification

--- main.c
+++ main.c
@@ -3,47 +3,41 @@
 
 int main()
 {
-     float nbr1, nbr2;
+    float nbr1, nbr2;
     char M, quitter;
-    do{
-    do
+
+    printf("pour quitter tape 'q' pour contunier tape autre chose\n");
+    scanf("\n%c", &quitter);
+    if(quitter != 'q')
+    {
+        do
     {
         printf("choisir l'opperateur : +, -, *, /\n");
-     scanf("\n%c", &M);
-    }while ( M != '+' && M != '-' && M != '*' && M != '/');
+    scanf("\n%c", &M);
+    }
+    while ( M != '+' && M != '-' && M != '*' && M != '/');
     printf("Saisir le 1er nombre\n");
     scanf("%f", &nbr1);
-   printf("Saisir un 2eme nombre\n");
+    printf("Saisir un 2eme nombre\n");
     scanf("%f", &nbr2);
 
     switch (M)
     {
-    case '+' : printf ("Resultat = %.2f\n", nbr1 + nbr2);
-			        break;
-		case '-' : printf ("Resultat = %.2f\n",nbr1 - nbr2);
-			break;
-		case '*' : printf ("Resultat= %.2f\n",nbr1 * nbr2);
-			break;
-		case '/' :
-		if (nbr2 != 0)
-		{
-		    printf("Resultat = %.2f\n", nbr1 / nbr2);
-		}
-		else
+        case '+' : printf ("Resultat = %.2f\n", nbr1 + nbr2);
+        break;
+        case '-' : printf ("Resultat = %.2f\n",nbr1 - nbr2);
+        break;
+        case '*' : printf ("Resultat= %.2f\n",nbr1 * nbr2);
+        break;
+        case '/' :
+            if (nbr2 != 0)
+                printf("Resultat = %.2f\n", nbr1 / nbr2);
+            else
 		printf("l'opperation est impossible\n");
-			break;
-	    case 'q' :
-	    break;
+		break;
 		default :
-		printf("Opperation invalid\n");
-
-    }
-
-    printf("pour quitter tape q\n");
-    scanf("\n%c", &quitter);
-    }
-    while (quitter != 'q');
-    exit(0);
-
-    return 0;
+		    printf("Opperation invalid\n");
+		    }
+		    }
+		    return 0;
 }
